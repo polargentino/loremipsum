5f449120c7b8c7712f484823b437eb099f2d3216
Mateo Berardo <mateoberardo2008@gmail.com>
2025-08-25 17:45:42 -0300

    agrego comentario de prueba

--- lipsum.sh
+++ lipsum.sh
@@ -4,9 +4,11 @@ do
     # Genera un número aleatorio de párrafos entre 1 y 5
     parrafos=$(shuf -i 1-5 -n 1)
 
-    # Llama a lipsum.com y guarda el contenido en una variable
-    lorem=$(curl -s "https://www.lipsum.com/feed/xml?paras=$parrafos" | grep '<lipsum>' | sed 's/<lipsum>//;s/<\/lipsum>//')
+    # Llama a lipsum.com y guarda el contenido, eliminando las etiquetas HTML
+    lorem=$(curl -s "https://www.lipsum.com/feed/xml?paras=$parrafos" | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed 's/<lipsum>//g;s/<\/lipsum>//g')
 
-    # Crea un archivo con el contenido
+    # Crea el archivo con el contenido
     echo "$lorem" > loremipsum-$i.txt
 done
+
+#agrego comentario de mateo
